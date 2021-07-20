102f4fcabc6829b4e9f2ce3fa57cf32f14ca6b51
“WenXuecheng” <connor_wen@qq.com>
2021-07-21 04:34:44 +0800

    1

--- wifi_dev/switch.py
+++ wifi_dev/switch.py
@@ -1,8 +1,4 @@
 import requests
-
-dev_name_and_ip={
-    "esp-switch-wifi-dev-toplight":'',
-}
 name_to_dev_name = {
     "顶灯":"esp-switch-wifi-dev-toplight",
 }
@@ -17,36 +13,39 @@ def option_dev_swtich(name,op,va,ip):
         name=name
     if name == "esp-switch-wifi-dev-toplight":
         if op == 'init':
-            dev_name_and_ip["esp-switch-wifi-dev-toplight"] = ip;
+            with open('./devs/esp-switch-wifi-dev-toplight','w') as fd:
+                fd.write(ip)
             return None;
         if op == 'open':
-            url = 'http://' + dev_name_and_ip[name] + '/' + op
-            res = requests.get(url)
-            res_switch = {
-                "name_switch": name,
-                "option_switch": op,
-                "status_switch": res.split(':')[1],
-            }
-            return res_switch
+            with open('./devs/esp-switch-wifi-dev-toplight','w') as fd:
+                url = 'http://' + fd.read() + '/' + op
+                res = requests.get(url)
+                res_switch = {
+                    "name_switch": name,
+                    "option_switch": op,
+                    "status_switch": res.split(':')[1],
+                }
+                return res_switch
         if op == 'close':
-            url = 'http://' + dev_name_and_ip[name] + '/' + op
-            res = requests.get(url)
-            res_switch = {
-                "name_switch": name,
-                "option_switch": op,
-                "status_switch": res.split(':')[1],
-            }
-            return res_switch
+            with open('./devs/esp-switch-wifi-dev-toplight','w') as fd:
+                url = 'http://' + fd.read() + '/' + op
+                res = requests.get(url)
+                res_switch = {
+                    "name_switch": name,
+                    "option_switch": op,
+                    "status_switch": res.split(':')[1],
+                }
+                return res_switch
         if op == 'get_status':
-            url = 'http://' + dev_name_and_ip[name] + '/' + op
-            res = requests.get(url)
-            res_switch = {
-                "name_switch": name,
-                "option_switch": op,
-                "status_switch": res.split(':')[1],
-            }
-            return res_switch
-
+            with open('./devs/esp-switch-wifi-dev-toplight','w') as fd:
+                url = 'http://' + fd.read() + '/' + op
+                res = requests.get(url)
+                res_switch = {
+                    "name_switch": name,
+                    "option_switch": op,
+                    "status_switch": res.split(':')[1],
+                }
+                return res_switch
 
 
 
