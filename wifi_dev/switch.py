539e6cb981e37747a3de0a7f6caf40de71a778e5
“WenXuecheng” <connor_wen@qq.com>
2021-07-21 05:06:34 +0800

    1

--- wifi_dev/switch.py
+++ wifi_dev/switch.py
@@ -1,5 +1,8 @@
 import requests
-import config
+
+dev_name_and_ip={
+    "esp-switch-wifi-dev-toplight":'',
+}
 name_to_dev_name = {
     "顶灯":"esp-switch-wifi-dev-toplight",
 }
@@ -14,39 +17,36 @@ def option_dev_swtich(name,op,va,ip):
         name=name
     if name == "esp-switch-wifi-dev-toplight":
         if op == 'init':
-            with open(config.settings["wifi_dev_path"]+'/devs/esp-switch-wifi-dev-toplight','w+') as fd:
-                fd.write(ip)
+            dev_name_and_ip["esp-switch-wifi-dev-toplight"] = ip;
             return None;
         if op == 'open':
-            with open(config.settings["wifi_dev_path"]+'/devs/esp-switch-wifi-dev-toplight','r') as fd:
-                url = 'http://' + fd.read() + '/' + op
-                res = requests.get(url)
-                res_switch = {
-                    "name_switch": name,
-                    "option_switch": op,
-                    "status_switch": res.split(':')[1],
-                }
-                return res_switch
+            url = 'http://' + dev_name_and_ip[name] + '/' + op
+            res = requests.get(url)
+            res_switch = {
+                "name_switch": name,
+                "option_switch": op,
+                "status_switch": res.split(':')[1],
+            }
+            return res_switch
         if op == 'close':
-            with open(config.settings["wifi_dev_path"]+'/devs/esp-switch-wifi-dev-toplight','r') as fd:
-                url = 'http://' + fd.read() + '/' + op
-                res = requests.get(url)
-                res_switch = {
-                    "name_switch": name,
-                    "option_switch": op,
-                    "status_switch": res.split(':')[1],
-                }
-                return res_switch
+            url = 'http://' + dev_name_and_ip[name] + '/' + op
+            res = requests.get(url)
+            res_switch = {
+                "name_switch": name,
+                "option_switch": op,
+                "status_switch": res.split(':')[1],
+            }
+            return res_switch
         if op == 'get_status':
-            with open(config.settings["wifi_dev_path"]+'/devs/esp-switch-wifi-dev-toplight','r') as fd:
-                url = 'http://' + fd.read() + '/' + op
-                res = requests.get(url)
-                res_switch = {
-                    "name_switch": name,
-                    "option_switch": op,
-                    "status_switch": res.split(':')[1],
-                }
-                return res_switch
+            url = 'http://' + dev_name_and_ip[name] + '/' + op
+            res = requests.get(url)
+            res_switch = {
+                "name_switch": name,
+                "option_switch": op,
+                "status_switch": res.split(':')[1],
+            }
+            return res_switch
+
 
 
 
