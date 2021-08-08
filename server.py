fd64adc4d3ae10a3e97db7466e6e5c0fedbe33d1
“WenXuecheng” <connor_wen@qq.com>
2021-08-09 01:41:06 +0800

    1

--- server.py
+++ server.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     app = Application()
     httpServer = tornado.httpserver.HTTPServer(app)
     httpServer.bind(config.options["port"])
-    httpServer.start(0)
+    httpServer.start(1)
     tornado.ioloop.IOLoop.current().start()
