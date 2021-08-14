4bba84ae7b7fcc78019fe5548f065dafab8f4b67
“WenXuecheng” <connor_wen@qq.com>
2021-08-14 21:09:09 +0800

    1

--- wifi_dev/lamp.py
+++ wifi_dev/lamp.py
@@ -4,7 +4,7 @@ dev_name_and_ip={
     "esp-lamp-wifi-dev-table-lamp":'',
 }
 name_to_dev_name = {
-    "台灯":"esp-switch-wifi-dev-table-lamp",
+    "台灯":"esp-lamp-wifi-dev-table-lamp",
 }
 
 
@@ -15,10 +15,10 @@ def option_dev_swtich(name,op,va,ip):
         name = name_to_dev_name[name]
     except:
         name=name
-    if name == "esp-switch-wifi-dev-table-lamp":
+    if name == "esp-lamp-wifi-dev-table-lamp":
         if op == 'init':
-            if ip != dev_name_and_ip["esp-switch-wifi-dev-table-lamp"]:
-                dev_name_and_ip["esp-switch-wifi-dev-table-lamp"] = ip
+            if ip != dev_name_and_ip["esp-lamp-wifi-dev-table-lamp"]:
+                dev_name_and_ip["esp-lamp-wifi-dev-table-lamp"] = ip
                 return 'ip changed'
             return 'init ok'
         if op == 'open':
