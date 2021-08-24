5b894c2cb6eaeae2a96c29540ca866369850ff5b
“WenXuecheng” <connor_wen@qq.com>
2021-08-24 21:47:46 +0800

    1

--- views/index.py
+++ views/index.py
@@ -192,4 +192,4 @@ class WindowCoveringHandler(RequestHandler):
 
 class TestHandler(RequestHandler):
     def get(self, *args, **kwargs):
-        self.write('{"title":"json在线解析（简版） -JSON在线解析","json.url":"https://www.sojson.com/simple_json.html","keywords":"json在线解析","功能":["JSON美化","JSON数据类型显示","JSON数组显示角标","高亮显示","错误提示",{"备注":["www.sojson.com","json.la"]}],"加入我们":{"qq群":"259217951"}}')
+        self.render('./test.html')
